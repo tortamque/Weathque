e6e4e8167d3df1ffa01fbb9a6b31b0b449a40f06
tortamque <90132962+tortamque@users.noreply.github.com>
2023-07-24 13:42:27 +0300

    Add WeeklyForecast to the menu

--- lib/features/app/presentation/widgets/core/menu.dart
+++ lib/features/app/presentation/widgets/core/menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weathque/features/app/presentation/widgets/card/weather_card.dart';
+import 'package:weathque/features/app/presentation/widgets/scrollable/weekly_forecast.dart';
 import 'package:weathque/features/app/presentation/widgets/texts/condition.dart';
 import 'package:weathque/features/app/presentation/widgets/texts/header.dart';
 import 'package:weathque/features/app/presentation/widgets/texts/date.dart';
@@ -25,6 +26,7 @@ class Menu extends StatelessWidget {
           const SummaryText(text: "Now it feels like +35\", actually +31.\nIt feels hot because of the direct sun. Today,\nthe temperature is felt in the range from +31\" to 27\"."),
           const WeatherCard(),
           const WeeklyForecastHeader(),
+          WeeklyForecast()
         ],
       ),
     );
