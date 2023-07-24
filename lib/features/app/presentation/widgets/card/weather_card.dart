af8a040a9f00c2eadaefcf22f41e47d0d95e3d06
tortamque <90132962+tortamque@users.noreply.github.com>
2023-07-24 11:36:09 +0300

    Add CardItem widgets to WeatherCard

--- lib/features/app/presentation/widgets/card/weather_card.dart
+++ lib/features/app/presentation/widgets/card/weather_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weathque/features/app/presentation/widgets/card/card_item.dart';
 
 class WeatherCard extends StatelessWidget {
   const WeatherCard({super.key});
@@ -13,6 +14,25 @@ class WeatherCard extends StatelessWidget {
         ),
         width: double.infinity,
         height: 125,
+        child: Row(
+          children: [
+            CardItem(
+              iconPath: "assets/icons/wind.png",
+              data: "4km/h", 
+              type: "Wind"
+            ),
+            CardItem(
+              iconPath: "assets/icons/drop.png",
+              data: "48%", 
+              type: "Humidity"
+            ),
+            CardItem(
+              iconPath: "assets/icons/eye.png",
+              data: "1.6km", 
+              type: "Visibility"
+            ),
+          ],
+        ),
       ),
     );
   }
