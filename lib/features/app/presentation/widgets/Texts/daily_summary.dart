749367c7bddfd8a2909886cfb08ed8d42c80f018
tortamque <90132962+tortamque@users.noreply.github.com>
2023-07-24 10:50:47 +0300

    Remove unnecessary padding fields

--- lib/features/app/presentation/widgets/Texts/daily_summary.dart
+++ lib/features/app/presentation/widgets/Texts/daily_summary.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
 
 class DailySummary extends StatelessWidget {
-  final double padding;
-
-  const DailySummary({required this.padding, super.key});
+  const DailySummary({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: padding),
-      child: const Text(
-        "Daily Summary",
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          fontSize: 18
-        ),
+    return const Text(
+      "Daily Summary",
+      style: TextStyle(
+        fontWeight: FontWeight.w800,
+        fontSize: 18
       ),
     );
   }
--- lib/features/app/presentation/widgets/Texts/summary_text.dart
+++ lib/features/app/presentation/widgets/Texts/summary_text.dart
@@ -2,23 +2,18 @@ import 'package:flutter/material.dart';
 
 class SummaryText extends StatelessWidget {
   final String text;
-  final double padding;
 
   const SummaryText({
     required this.text,
-    required this.padding,
     super.key
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: padding),
-      child: Text(
-        text,
-        style: const TextStyle(
-          fontWeight: FontWeight.bold,
-        ),
+    return Text(
+      text,
+      style: const TextStyle(
+        fontWeight: FontWeight.bold,
       ),
     );
   }
