64f6dbf8afaa7582152a96baa1ab4ae018d38f7d
tortamque <90132962+tortamque@users.noreply.github.com>
2023-07-24 10:57:37 +0300

    Create WeatherCard widget

---
+++ lib/features/app/presentation/widgets/card/weather_card.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WeatherCard extends StatelessWidget {
+  const WeatherCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: Colors.black
+        ),
+        width: double.infinity,
+        height: 125,
+      ),
+    );
+  }
+}
--- lib/features/app/presentation/widgets/core/menu.dart
+++ lib/features/app/presentation/widgets/core/menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weathque/features/app/presentation/widgets/card/weather_card.dart';
 import 'package:weathque/features/app/presentation/widgets/texts/condition.dart';
 import 'package:weathque/features/app/presentation/widgets/texts/daily_summary.dart';
 import 'package:weathque/features/app/presentation/widgets/texts/date.dart';
@@ -21,6 +22,7 @@ class Menu extends StatelessWidget {
           const Temperature(temperature: "31"),
           const DailySummary(),
           const SummaryText(text: "Now it feels like +35\", actually +31.\nIt feels hot because of the direct sun. Today,\nthe temperature is felt in the range from +31\" to 27\"."),
+          WeatherCard()
         ],
       ),
     );
