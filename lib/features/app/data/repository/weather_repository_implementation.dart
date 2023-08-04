24cc49c459214d8afcb4e3591a40256347fb8aef
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-04 16:24:20 +0300

    Implement repository

--- lib/features/app/data/repository/weather_repository_implementation.dart
+++ lib/features/app/data/repository/weather_repository_implementation.dart
@@ -1,9 +1,17 @@
+import 'package:weathque/core/sensitive.dart';
+import 'package:weathque/features/app/data/data_sources/remote/api_service.dart';
 import 'package:weathque/features/app/data/models/weather_model.dart';
 import 'package:weathque/features/app/domain/repository/weather_repository.dart';
 
 class WeatherRepositoryImplementation implements WeatherRepository{
+  final CurrentWeatherApiService _currentWeatherApiService;
+
+  WeatherRepositoryImplementation(this._currentWeatherApiService);
+
   @override
-  Future<WeatherModel> getCurrentWeather() {
-    throw UnimplementedError();
+  Future<WeatherModel> getCurrentWeather(String latitude, String longitude) async {
+    final httpResponse = await _currentWeatherApiService.getCurrentWeather(latitude: latitude, longitude: longitude, apiKey: apiKey);
+
+    return httpResponse.data;
   }
 }
