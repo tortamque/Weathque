d9b39bc818f767603c1d334eb22a1838c709ed11
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-06 21:05:47 +0300

    Add bloc to fetch the API

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weathque/config/theme/app_themes.dart';
-import 'package:weathque/config/theme/custom_colors.dart';
 import 'package:weathque/core/dependency_injection.dart';
+import 'package:weathque/features/app/presentation/bloc/get_current_weather_bloc.dart';
+import 'package:weathque/features/app/presentation/bloc/get_current_weather_event.dart';
+import 'package:weathque/features/app/presentation/bloc/get_current_weather_state.dart';
+import 'package:weathque/features/app/presentation/pages/loading_page.dart';
 import 'package:weathque/features/app/presentation/pages/paris.dart';
-import 'package:weathque/features/app/presentation/widgets/appBar/appbar.dart';
-import 'package:weathque/features/app/presentation/widgets/core/menu.dart';
-
 void main() {
   initializeDependencies();
   runApp(const MyApp());
@@ -15,9 +16,22 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: theme(),
-      home: ParisPage()
+    return BlocProvider<GetCurrentWeatherBloc>(
+      create: (context) => locator()..add(const GetCurrentWeather(cityName: "Paris")),
+        child: MaterialApp(
+          theme: theme(),
+          home: BlocBuilder<GetCurrentWeatherBloc, GetCurrentWeatherState>(
+            builder: (_, state) {
+              if(state is GetCurrentWeatherLoading){
+                return const LoadingPage();
+              }
+              if(state is GetCurrentWeatherDone){
+                return ParisPage();
+              }
+              return const SizedBox();
+            },
+          ),
+      )
     );
   }
 }
