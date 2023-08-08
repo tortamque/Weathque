0fe4d884e03e9d24b023b00dc19e5d6e0339322a
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-08 14:37:27 +0300

    Create model for weather forecast

---
+++ lib/features/app/data/models/forecast_weather_model.dart
@@ -0,0 +1,31 @@
+import 'package:weathque/features/app/domain/entities/forecast_weather_entity.dart';
+
+class ForecastWeatherModel extends ForecastWeatherEntity {
+  ForecastWeatherModel({
+    required cod,
+    required message,
+    required cnt,
+    required list,
+    required city,
+  }):super(
+    cod: cod,
+    message: message,
+    cnt: cnt,
+    list: list,
+    city: city
+  );
+
+  factory ForecastWeatherModel.fromJson(Map<String, dynamic> json) {
+    List<WeatherData> weatherDataList = (json['list'] as List)
+        .map((data) => WeatherData.fromJson(data))
+        .toList();
+
+    return ForecastWeatherModel(
+      cod: json['cod'],
+      message: json['message'],
+      cnt: json['cnt'],
+      list: weatherDataList,
+      city: City.fromJson(json['city']),
+    );
+  }
+}
