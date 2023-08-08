4f336d91fc5f30bc519ba8e8e7c3bb386fa952b8
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-08 17:23:51 +0300

    Move current weather bloc into separate folder

--- lib/features/app/presentation/bloc/get_current_weather/get_current_weather_bloc.dart
+++ lib/features/app/presentation/bloc/get_current_weather/get_current_weather_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weathque/features/app/domain/usecases/get_current_weather.dart';
-import 'package:weathque/features/app/presentation/bloc/get_current_weather_event.dart';
-import 'package:weathque/features/app/presentation/bloc/get_current_weather_state.dart';
+import 'package:weathque/features/app/presentation/bloc/get_current_weather/get_current_weather_event.dart';
+import 'package:weathque/features/app/presentation/bloc/get_current_weather/get_current_weather_state.dart';
 
 class GetCurrentWeatherBloc extends Bloc<GetCurrentWeatherEvent, GetCurrentWeatherState>{
   final GetCurrentWeatherUseCase _getCurrentWeatherUseCase;
@@ -12,7 +12,7 @@ class GetCurrentWeatherBloc extends Bloc<GetCurrentWeatherEvent, GetCurrentWeath
 
   void onGetCurrentWeather(GetCurrentWeather event, Emitter<GetCurrentWeatherState> emitter) async{
     final dataState = await _getCurrentWeatherUseCase.call(cityName: event.cityName);
-
+    
     // ignore: invalid_use_of_visible_for_testing_member
     emit(
       GetCurrentWeatherDone(dataState)
