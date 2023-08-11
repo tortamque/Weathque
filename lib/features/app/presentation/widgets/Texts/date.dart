dc330d20833dbec5e7a367753580c59f7fc0fb67
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-11 15:56:09 +0300

    Add animation and date card

--- lib/features/app/presentation/widgets/Texts/date.dart
+++ lib/features/app/presentation/widgets/Texts/date.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:weathque/config/theme/custom_colors.dart';
+import 'package:weathque/features/app/presentation/widgets/animations/date/date_animation.dart';
+import 'package:weathque/features/app/presentation/widgets/card/date_card.dart';
 
-class Date extends StatelessWidget {
+class Date extends StatefulWidget {
   final String date;
 
   const Date({
@@ -10,25 +12,76 @@ class Date extends StatelessWidget {
   });
 
   @override
+  State<Date> createState() => _DateState();
+}
+
+class _DateState extends State<Date> {
+  final Duration animationDuration = const Duration(milliseconds: 500);
+  final Curve animationCurve = Curves.easeOutSine;
+  bool isTextAnimated = false;
+  bool isContainerAnimated = false;
+
+  @override
+  void initState() {
+    _startContainerAnimation();
+    _startTextAnimation();
+    super.initState();
+  }
+
+  void _startTextAnimation() async {
+    await Future.delayed(const Duration(milliseconds: 500))
+      .then((value) => setState(() => isTextAnimated = true));
+  }
+
+  void _startContainerAnimation() async {
+    await Future.delayed(const Duration(seconds: 0))
+      .then((value) => setState(() => isContainerAnimated = true));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          color: CustomColors.black
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 15,
-            vertical: 5 
+      child: Stack(
+        children: [
+          DateCard(
+            backgroundColor: Colors.transparent,
+            textColor: Colors.transparent,
+            text: widget.date,
           ),
-          child: Text(
-            date,
-            style: const TextStyle(
-              color: CustomColors.yellow,
+
+          DateAnimation(
+            curve: animationCurve, 
+            duration: animationDuration, 
+            animationField: isContainerAnimated, 
+            positionInitialValue: 20, 
+            opacityInitialValue: 0,
+            child: DateCard(
+              backgroundColor: CustomColors.black,
+              textColor: Colors.transparent,
+              text: widget.date,
             ),
           ),
-        ),
+
+          DateAnimation(
+            curve: animationCurve, 
+            duration: animationDuration, 
+            animationField: isTextAnimated, 
+            positionInitialValue: 20, 
+            opacityInitialValue: 0,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 5 
+              ),
+              child: Text(
+                widget.date,
+                style: const TextStyle(
+                  color: CustomColors.yellow,
+                ),
+              ),
+            )
+          )
+        ],
       ),
     );
   }
