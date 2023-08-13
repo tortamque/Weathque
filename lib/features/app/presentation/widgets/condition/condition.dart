99b63948694263523f6794c39b5ed82c80ea917f
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-13 13:29:26 +0300

    Implement animation

--- lib/features/app/presentation/widgets/condition/condition.dart
+++ lib/features/app/presentation/widgets/condition/condition.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:weathque/config/theme/custom_colors.dart';
+import 'package:weathque/features/app/presentation/widgets/condition/condition_animation.dart';
+import 'package:weathque/features/app/presentation/widgets/condition/condition_text.dart';
 
-class Condition extends StatelessWidget {
+class Condition extends StatefulWidget {
   final String condition;
 
   const Condition({
@@ -10,17 +12,49 @@ class Condition extends StatelessWidget {
   });
 
   @override
+  State<Condition> createState() => _ConditionState();
+}
+
+class _ConditionState extends State<Condition> {
+  final Duration animationDuration = const Duration(milliseconds: 500);
+  final Curve animationCurve = Curves.easeOutSine;
+  bool isAnimated = false;
+  
+  @override
+  void initState() {
+    _startAnimation();
+    super.initState();
+  }
+
+  void _startAnimation() async{
+    await Future.delayed(const Duration(milliseconds: 600))
+      .then((value) => setState(() => isAnimated = true));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 10),
-        child: Text(
-          condition,
-          style: const TextStyle(
-            color: CustomColors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 16
-          ),
+        child: Stack(
+          children: [
+            ConditionText(
+              text: widget.condition,
+              color: Colors.transparent,
+            ),
+
+            ConditionAnimation(
+              isAnimated: isAnimated, 
+              animationDuration: animationDuration, 
+              animationCurve: animationCurve, 
+              positionInitialValue: 20, 
+              opacityInitialValue: 0,
+              child: ConditionText(
+                text: widget.condition,
+                color: CustomColors.black,
+              ),
+            )
+          ],
         ),
       ),
     );
