8492057609ef367f3175d084299ebb5826a2071e
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-16 14:03:02 +0300

    Add cities to the CarouselSlider

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_carousel_slider/carousel_slider.dart';
 import 'package:weathque/config/theme/app_themes.dart';
 import 'package:weathque/core/dependency_injection.dart';
-import 'package:weathque/features/app/domain/entities/current_city_entity.dart';
 import 'package:weathque/features/app/presentation/bloc/get_current_weather/get_current_weather_bloc.dart';
 import 'package:weathque/features/app/presentation/bloc/get_current_weather/get_current_weather_event.dart';
 import 'package:weathque/features/app/presentation/bloc/get_current_weather/get_current_weather_state.dart';
@@ -12,6 +11,8 @@ import 'package:weathque/features/app/presentation/bloc/get_weather_forecast/get
 import 'package:weathque/features/app/presentation/bloc/get_weather_forecast/get_weather_forecast_state.dart';
 import 'package:weathque/features/app/presentation/pages/loading_page.dart';
 import 'package:weathque/features/app/presentation/pages/weather_page.dart';
+
+import 'features/app/domain/entities/cities_enum.dart';
 void main() {
   initializeDependencies();
   runApp(const MyApp());
@@ -24,10 +25,10 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider<GetCurrentWeatherBloc>(
-          create: (context) => locator()..add(GetCurrentWeather(cityName: locator<CurrentCity>().currentCity.string))
+          create: (context) => locator()..add(GetCurrentWeather())
         ),
         BlocProvider<GetWeatherForecastBloc>(
-          create: (context) => locator()..add(GetWeatherForecast(cityName: locator<CurrentCity>().currentCity.string)),
+          create: (context) => locator()..add(GetWeatherForecast()),
         )
       ], 
       child: MaterialApp(
@@ -39,7 +40,6 @@ class MyApp extends StatelessWidget {
                 return const LoadingPage();
               }
               if(currentWeatherState is GetCurrentWeatherDone){
-                // GetWeatherForecastBloc
                 return BlocBuilder<GetWeatherForecastBloc, GetWeatherForecastState>(
                   builder: (_, forecastWeatherState) {
                     if(forecastWeatherState is GetWeatherForecastLoading){
@@ -47,21 +47,15 @@ class MyApp extends StatelessWidget {
                     }
                     if(forecastWeatherState is GetWeatherForecastDone){
                       return CarouselSlider(
-                        onSlideChanged: (value) {
-                          print(value);
-                        },
                         slideTransform: const CubeTransform(),
                         unlimitedMode: true,
                         children: [
-                          WeatherPage(
-                            weatherEntity: currentWeatherState.weatherEntity,
-                            forecastWeatherEntity: forecastWeatherState.forecastWeatherEntity,
-                          ),
-                          WeatherPage(
-                            weatherEntity: currentWeatherState.weatherEntity,
-                            forecastWeatherEntity: forecastWeatherState.forecastWeatherEntity,
-                          )
-                        ]
+                          for (var city in City.values)
+                            WeatherPage(
+                              weatherEntity: currentWeatherState.weatherEntity![city.string]!,
+                              forecastWeatherEntity: forecastWeatherState.forecastWeatherEntity![city.string]!,
+                            ),
+                        ],
                       );
                     }
                     return const SizedBox();
@@ -69,8 +63,8 @@ class MyApp extends StatelessWidget {
                 );
               }
               return const SizedBox();
-            },
-          ),
+            }
+          )
       )
     );
   }
