8435dbf581c1350043c7614c8785ca6485c3da08
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-19 19:53:48 +0300

    Create implementation of SaveCity usecase

---
+++ lib/features/app/domain/usecases/save_city.dart
@@ -0,0 +1,13 @@
+import 'package:weathque/core/usecases/usecase.dart';
+import 'package:weathque/features/app/domain/repository/storage_repository.dart';
+
+class SaveCityUseCaseImplementation implements SaveCityUseCase{
+  final StorageRepository _storage;
+
+  SaveCityUseCaseImplementation(this._storage);
+  
+  @override
+  Future<void> call({required String cityName}) {
+    return _storage.saveCity(cityName);
+  }
+}
