82fbad0588507ff12350e0aa3004c79698449321
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-20 23:45:42 +0300

    Create getCities method in the Storage

--- lib/features/app/data/data_sources/local/storage.dart
+++ lib/features/app/data/data_sources/local/storage.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 abstract class Storage {
   late SharedPreferences prefs;
   Future<void> saveCity(String cityName);
+  List<String>? getCities();
 }
 
 class StorageImplementation implements Storage{
@@ -23,5 +24,12 @@ class StorageImplementation implements Storage{
     // P.S. This is just a template, I'm aware that it doesn't work as planned. 
     // Will fix it later
     await prefs.setStringList('cities', [cityName]);
+  }
+  
+  @override
+  List<String>? getCities() {
+    final List<String>? items = prefs.getStringList('cities');
+
+    return items;
   }
 }
