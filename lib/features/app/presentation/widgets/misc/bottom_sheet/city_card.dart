6b0a4c51c966402fb09982ad1aa0886b221154f7
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-23 21:22:52 +0300

    Add animation to CityCard

--- lib/features/app/presentation/widgets/misc/bottom_sheet/city_card.dart
+++ lib/features/app/presentation/widgets/misc/bottom_sheet/city_card.dart
@@ -1,39 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:weathque/config/theme/custom_colors.dart';
 import 'package:weathque/core/dependency_injection.dart';
 import 'package:weathque/features/app/domain/usecases/delete_city.dart';
 import 'package:weathque/features/app/presentation/bloc/add_city/cities_changed_cubit.dart';
+import 'package:weathque/features/app/presentation/widgets/animations/top_animation.dart';
 
-class CityCard extends StatelessWidget {
+class CityCard extends StatefulWidget {
   final String name;
+  final int index;
 
-  const CityCard({super.key, required this.name});
+  const CityCard({super.key, required this.name, required this.index});
 
   @override
-  Widget build(BuildContext context)  {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 2.5),
-      child: Dismissible(
-        key: Key(name),
-        onDismissed: (_) async {
-          await locator<DeleteCityUseCaseImplementation>()(name);
-          context.read<CitiesChangedCubit>()();
-        },
-        child: Card(
+  State<CityCard> createState() => _CityCardState();
+}
+
+class _CityCardState extends State<CityCard> {
+  bool isAnimated = false;
+  final Duration animationDuration = const Duration(milliseconds: 750);
+  final Curve animationCurve = Curves.easeOutSine;
+  final int animationStep = 200;
+
+  @override
+  void initState() {
+    _animate();
+    super.initState();
+  }
+
+  void _animate() async{
+    await Future.delayed(Duration(milliseconds: (widget.index + 1) * animationStep))
+      .then((value) => setState(() => isAnimated = true));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SizedBox(
           child: Center(
             child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15),
+              padding: const EdgeInsets.symmetric(vertical: 20),
               child: Text(
-                name,
+                widget.name,
                 style: TextStyle(
                   fontSize: 24,
+                  color: Colors.transparent
                 ),
               )
             )
           ),
         ),
-      ),
+
+        TopAnimation(
+          curve: animationCurve, 
+          duration: animationDuration, 
+          animationField: isAnimated, 
+          positionInitialValue: MediaQuery.of(context).size.height/11, 
+          opacityInitialValue: 1, 
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 2.5),
+            child: Dismissible(
+              key: Key(widget.name),
+              onDismissed: (_) async {
+                await locator<DeleteCityUseCaseImplementation>()(widget.name);
+                context.read<CitiesChangedCubit>()();
+              },
+              child: Card(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    child: Text(
+                      widget.name,
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    )
+                  )
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
