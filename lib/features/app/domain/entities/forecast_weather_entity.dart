65a655b569ddcef718e60641486bb3ab5dee0ad5
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-24 17:05:50 +0300

    Make coordinated double

--- lib/features/app/domain/entities/forecast_weather_entity.dart
+++ lib/features/app/domain/entities/forecast_weather_entity.dart
@@ -199,6 +199,6 @@ class Coord {
   Coord({required this.lat, required this.lon});
 
   factory Coord.fromJson(Map<String, dynamic> json) {
-    return Coord(lat: json['lat'], lon: json['lon']);
+    return Coord(lat: json['lat'].toDouble(), lon: json['lon'].toDouble());
   }
 }
