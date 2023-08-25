56366c77cc00548b3541322a707ca2938b7e894d
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-25 18:04:27 +0300

    Move MultiBlocProvider into separate windet

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:weathque/config/theme/app_themes.dart';
 import 'package:weathque/core/dependency_injection.dart';
 import 'package:weathque/features/app/data/data_sources/local/storage.dart';
-import 'package:weathque/features/app/presentation/bloc/add_city/cities_changed_cubit.dart';
-import 'package:weathque/features/app/presentation/bloc/get_current_weather/get_current_weather_bloc.dart';
-import 'package:weathque/features/app/presentation/bloc/get_current_weather/get_current_weather_event.dart';
-import 'package:weathque/features/app/presentation/bloc/get_weather_forecast/get_weather_forecast_bloc.dart';
-import 'package:weathque/features/app/presentation/bloc/get_weather_forecast/get_weather_forecast_event.dart';
+import 'package:weathque/features/app/presentation/bloc/blocs_provider_builder.dart';
 import 'package:weathque/features/app/presentation/bloc/widgets/cities_cubit_widget.dart';
 import 'package:weathque/features/app/presentation/bloc/widgets/current_weather_bloc_widget.dart';
 import 'package:weathque/features/app/presentation/bloc/widgets/forecast_bloc_widget.dart';
@@ -25,18 +20,8 @@ class WeathqueApp extends StatelessWidget {
   const WeathqueApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<GetCurrentWeatherBloc>(
-          create: (context) => locator()..add(const GetCurrentWeather())
-        ),
-        BlocProvider<GetWeatherForecastBloc>(
-          create: (context) => locator()..add(const GetWeatherForecast()),
-        ),
-        BlocProvider<CitiesChangedCubit>(
-          create: (context) => locator(),
-        )
-      ], 
+    return BlocsProviderBuilder(
+      context: context,
       child: MaterialApp(
         builder: FToastBuilder(),
         theme: theme(),
@@ -53,7 +38,7 @@ class WeathqueApp extends StatelessWidget {
             );
           },
         )
-      )
+      ),
     );
   }
 }
