aa83c64c3d79174e9d0362aaac29d730faee9412
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-25 17:54:29 +0300

    Implement all the changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_carousel_slider/carousel_slider.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:weathque/config/theme/app_themes.dart';
-import 'package:weathque/config/theme/custom_colors.dart';
 import 'package:weathque/core/dependency_injection.dart';
 import 'package:weathque/features/app/data/data_sources/local/storage.dart';
-import 'package:weathque/features/app/domain/usecases/get_cities.dart';
-import 'package:weathque/features/app/domain/usecases/get_colors.dart';
 import 'package:weathque/features/app/presentation/bloc/add_city/cities_changed_cubit.dart';
 import 'package:weathque/features/app/presentation/bloc/get_current_weather/get_current_weather_bloc.dart';
 import 'package:weathque/features/app/presentation/bloc/get_current_weather/get_current_weather_event.dart';
-import 'package:weathque/features/app/presentation/bloc/get_current_weather/get_current_weather_state.dart';
 import 'package:weathque/features/app/presentation/bloc/get_weather_forecast/get_weather_forecast_bloc.dart';
 import 'package:weathque/features/app/presentation/bloc/get_weather_forecast/get_weather_forecast_event.dart';
-import 'package:weathque/features/app/presentation/bloc/get_weather_forecast/get_weather_forecast_state.dart';
-import 'package:weathque/features/app/presentation/pages/loading_page.dart';
-import 'package:weathque/features/app/presentation/pages/weather_page.dart';
+import 'package:weathque/features/app/presentation/bloc/widgets/cities_cubit_widget.dart';
+import 'package:weathque/features/app/presentation/bloc/widgets/current_weather_bloc_widget.dart';
+import 'package:weathque/features/app/presentation/bloc/widgets/forecast_bloc_widget.dart';
+import 'features/app/presentation/functions/build_carousel_slider.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -44,43 +40,16 @@ class WeathqueApp extends StatelessWidget {
       child: MaterialApp(
         builder: FToastBuilder(),
         theme: theme(),
-        home: BlocBuilder<CitiesChangedCubit, List<String>>(
-          builder: (_, cubitCities) {
-            return BlocBuilder<GetCurrentWeatherBloc, GetCurrentWeatherState>(
-              builder: (_, currentWeatherState) {
-                if(currentWeatherState is GetCurrentWeatherLoading){
-                  return LoadingPage(color: CustomColors.yellow.color);
-                }
-                if(currentWeatherState is GetCurrentWeatherDone){
-                  return BlocBuilder<GetWeatherForecastBloc, GetWeatherForecastState>(
-                    builder: (_, forecastWeatherState) {
-                      if(forecastWeatherState is GetWeatherForecastLoading){
-                        return LoadingPage(color: CustomColors.yellow.color);
-                      }
-                      if(forecastWeatherState is GetWeatherForecastDone){
-                        List<String> cities = locator<GetCitiesUseCaseImplementation>()();
-                        List<String> colors = locator<GetColorsUseCaseImplementation>()();
-                        
-                        return CarouselSlider(
-                          slideTransform: const CubeTransform(),
-                          unlimitedMode: true,
-                          children: [
-                            for (int i = 0; i < cities.length; i++)
-                              WeatherPage(
-                                weatherEntity: currentWeatherState.weatherEntity![cities[i]]!,
-                                forecastWeatherEntity: forecastWeatherState.forecastWeatherEntity![cities[i]]!,
-                                color: Color(int.parse(colors[i])),
-                                city: cities[i],
-                              ),
-                          ],
-                        );
-                      }
-                      return const SizedBox();
-                    },
-                  );
-                }
-                return const SizedBox();
-              }
+        home: CitiesCubitWidget(
+          onSuccess: (state) {
+            return CurrentWeatherForecastBloc(
+              onSuccess: (currentWeatherState) {
+                return ForecastBlocWidget(
+                  onSuccess: (forecastWeatherState) {
+                    return buildCarouselSlider(currentWeatherState, forecastWeatherState);
+                  },
+                );
+              },
             );
           },
         )
